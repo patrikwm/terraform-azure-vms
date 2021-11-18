0772c2764e1303e47035aa300a0604040c93713c
Patrik Wigur Martin <patrik.martin@mideye.com>
2021-11-18 16:47:17 +0100

    fix spelling

--- rhel8/variables.tf
+++ rhel8/variables.tf
@@ -1,7 +1,7 @@
 variable "location"               { default = "northeurope" }
 variable "prefix"                 { default = "rhel8" }
-variable "vm_admin"               { dafault = "azureuser" }
-varialbe "id_rsa_pub"             { default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCeg7RWgFddZN6QalA0RuWip5zGOhtDqILpSjD0c35KybSLj+lBxoQ4Gm1GwhHuTn4npj/I2KayZadfsEUUQ9AvfpI/cJFKefeqyCO1uroS2rETd+S0eoMizoJHEoQ7+h3PTvuzrY+ovwrvB9k/FC9VHwovPMJK7IThyf2rW5IStDU1Do/O/KR7jS/ctIobHlZFn77+Ri78k1pCqw78IixSRmKnKyrZa07uTyr6NpDujiuY/TMumnF5sN1LHMP+Tp3JgOOmMvpwGcWVRI2zBgBNy+hBnDDJ9jsgdBoCON1HA+LNyKe6oPyYwnRT6pq+6tuAs3gyAKs73nu3WWB6waYb" }
+variable "vm_admin"               { default = "azureuser" }
+variable "id_rsa_pub"             { default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCeg7RWgFddZN6QalA0RuWip5zGOhtDqILpSjD0c35KybSLj+lBxoQ4Gm1GwhHuTn4npj/I2KayZadfsEUUQ9AvfpI/cJFKefeqyCO1uroS2rETd+S0eoMizoJHEoQ7+h3PTvuzrY+ovwrvB9k/FC9VHwovPMJK7IThyf2rW5IStDU1Do/O/KR7jS/ctIobHlZFn77+Ri78k1pCqw78IixSRmKnKyrZa07uTyr6NpDujiuY/TMumnF5sN1LHMP+Tp3JgOOmMvpwGcWVRI2zBgBNy+hBnDDJ9jsgdBoCON1HA+LNyKe6oPyYwnRT6pq+6tuAs3gyAKs73nu3WWB6waYb" }
 variable "vm_publisher"           { default = "RedHat"}
 variable "vm_offer"               { default = "RHEL"}
 variable "vm_sku"                 { default = "8_4"}
