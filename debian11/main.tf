ef371367d3d08b5a1928c5b7080c432108842fa2
Patrik Wigur Martin <patrik.martin@mideye.com>
2021-11-19 10:25:05 +0100

    fix nic variable

--- debian11/main.tf
+++ debian11/main.tf
@@ -150,7 +150,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
     name                  = "${var.prefix}-VM"
     location              = var.location
     resource_group_name   = azurerm_resource_group.ResourceGroup.name
-    network_interface_ids = [azurerm_network_interface.myterraformnic.id]
+    network_interface_ids = [azurerm_network_interface.nic.id]
     size                  = "Standard_DS1_v2"
 
     os_disk {
