c396cd19260aa775c293e7ac7c59656ff67699ca
Patrik Wigur Martin <patrik.martin@mideye.com>
2021-11-26 14:16:00 +0100

    Working debian10

--- debian10/main.tf
+++ debian10/main.tf
@@ -135,15 +135,6 @@ resource "azurerm_storage_account" "StorageAccount" {
     }
 }
 
-# Create (and display) an SSH key
-# resource "tls_private_key" "example_ssh" {
-#   algorithm = "RSA"
-#   rsa_bits = 4096
-# }
-# output "tls_private_key" {
-#     value = tls_private_key.example_ssh.private_key_pem
-#     sensitive = true
-# }
 
 # Create virtual machine
 resource "azurerm_linux_virtual_machine" "vm" {
