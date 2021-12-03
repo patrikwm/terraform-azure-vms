b724ae664fe783054b5507f172539af4a346f079
Patrik Wigur Martin <patrik.martin@mideye.com>
2021-12-03 14:16:25 +0100

    Add Win2016 MSSQL express

---
+++ Win2016DatacenterMSSQL/variables.tf
@@ -0,0 +1,9 @@
+variable "location"               { default = "northeurope" }
+variable "prefix"                 { default = "windows2016" }
+variable "vm_admin"               { default = "azureuser" }
+variable "vm_password"            { default = "M3GaPASSWORD!" }
+variable "vm_publisher"           { default = "MicrosoftSQLServer" }
+variable "vm_offer"               { default = "SQL2017-WS2016" }
+variable "vm_sku"                 { default = "sqldev-gen2" }
+variable "vm_version"             { default = "latest" }
+variable "vm_size"                { default = "Standard_DS2_v2" }
